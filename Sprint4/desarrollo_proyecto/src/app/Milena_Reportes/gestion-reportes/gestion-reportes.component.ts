dded327630b36d1dc040f2808c85842ce56eb428
unknown <milechacom@gmail.com>
2021-10-22 16:56:17 -0500

    MilenaReportes

---
+++ Sprint4/desarrollo_proyecto/src/app/Milena_Reportes/gestion-reportes/gestion-reportes.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-reportes',
+  templateUrl: './gestion-reportes.component.html',
+  styleUrls: ['./gestion-reportes.component.css']
+})
+export class GestionReportesComponent implements OnInit { 
+    constructor() {}
+    ngOnInit(): void{} 
+}
