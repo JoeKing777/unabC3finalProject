ee4adf4f6d8f6364f0de69212d236f051342880b
unknown <milechacom@gmail.com>
2021-10-22 17:09:25 -0500

    MilenaListas

---
+++ Sprint4/desarrollo_proyecto/src/app/Milena_Reportes/listadecursos/listadecursos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listadecursos',
+  templateUrl: './listadecursos.component.html',
+  styleUrls: ['./listadecursos.component.css']
+})
+export class ListadecursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
