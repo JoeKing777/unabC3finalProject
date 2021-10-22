af7d7b1810f68dfa501c6b0fb34b21390ee8800a
Sami253 <samirenter15@gmail.com>
2021-10-22 18:30:02 -0500

    Funcionalidad Gestionar usuario

---
+++ Sprint4/desarrollo_proyecto/src/app/Funci_Sandra_Milena/cambiar/cambiar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CambiarComponent } from './cambiar.component';
+
+describe('CambiarComponent', () => {
+  let component: CambiarComponent;
+  let fixture: ComponentFixture<CambiarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CambiarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CambiarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Sprint4/desarrollo_proyecto/src/app/Funci_Sandra_Milena/gestionar-actualizar-i/gestionar-actualizar-i.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestionar-actualizar-i',
+  templateUrl: './gestionar-actualizar-i.component.html',
+  styleUrls: ['./gestionar-actualizar-i.component.css']
+})
+export class GestionarActualizarIComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
